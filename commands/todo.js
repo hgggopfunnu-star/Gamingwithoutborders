27d2a79e8acf89da4b128fc6724cb19ff4c7df94
FroxenMC <hgggopfunnu@gmail.com>
2026-03-22 15:18:10 +0530

    Create todo.js

---
+++ commands/todo.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const file = "./data/todo.json";
+
+if (!fs.existsSync(file))
+  fs.writeFileSync(file, "{}");
+
+function get() {
+  return JSON.parse(fs.readFileSync(file));
+}
+
+function save(d) {
+  fs.writeFileSync(file, JSON.stringify(d, null, 2));
+}
+
+module.exports = { get, save };
