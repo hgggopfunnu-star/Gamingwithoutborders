5b7d5ce64eb7bb20d6161d5dd17bb23a809dcde4
FroxenMC <hgggopfunnu@gmail.com>
2026-03-22 15:11:11 +0530

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,5 +1,7 @@
-const { EmbedBuilder, PermissionsBitField } = require("discord.js");
+const { PermissionsBitField } = require("discord.js");
 const ms = require("ms");
+
+const { ok, fail } = require("../utils/ui");
 
 module.exports = {
 
@@ -8,20 +10,30 @@ module.exports = {
   async execute(message, args) {
 
     if (!message.member.permissions.has(PermissionsBitField.Flags.ModerateMembers))
-      return;
+      return message.reply({ embeds: [fail("No permission")] });
 
     const user = message.mentions.members.first();
     const time = args[1];
+    const reason = args.slice(2).join(" ") || "No reason";
 
-    if (!user || !time) return;
+    if (!user)
+      return message.reply({ embeds: [fail("Mention user")] });
 
-    await user.timeout(ms(time));
+    if (!time)
+      return message.reply({ embeds: [fail("Give time")] });
 
-    const embed = new EmbedBuilder()
-      .setColor(0xffcc00)
-      .setDescription(`Muted ${user.user.tag} for ${time}`);
+    const duration = ms(time);
 
-    message.reply({ embeds: [embed] });
+    if (!duration)
+      return message.reply({ embeds: [fail("Invalid time")] });
+
+    await user.timeout(duration, reason);
+
+    message.reply({
+      embeds: [
+        ok(`${user.user.tag} muted for ${time}\nReason: ${reason}`)
+      ]
+    });
 
   }
 
