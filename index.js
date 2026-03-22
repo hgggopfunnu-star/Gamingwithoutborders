452920f4003660cd8b73b5c38f7ef2735df5842a
FroxenMC <hgggopfunnu@gmail.com>
2026-03-22 11:19:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,31 +3,19 @@ const { Client, GatewayIntentBits } = require("discord.js");
 const prefix = "&";
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+  intents: [GatewayIntentBits.Guilds]
 });
 
 client.once("ready", () => {
-  console.log("✅ Bot online");
+  console.log("Bot online");
 });
 
 client.on("messageCreate", (message) => {
+  if (!message.content) return;
 
-  if (message.author.bot) return;
-
-  if (!message.content.startsWith(prefix)) return;
-
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const cmd = args.shift().toLowerCase();
-
-
-  if (cmd === "ping") {
+  if (message.content === "&ping") {
     message.reply("pong");
   }
-
 });
 
 client.login(process.env.TOKEN);
