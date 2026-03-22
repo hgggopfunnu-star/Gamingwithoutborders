f96c859d3607a7024ba45eaba048ddbae5893c6d
FroxenMC <hgggopfunnu@gmail.com>
2026-03-22 15:22:01 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,48 +8,48 @@ module.exports = {
 
     const embed = new EmbedBuilder()
       .setColor(0x5865F2)
-      .setTitle("GamingWithoutBorders Bot")
-      .setDescription("Command List")
+      .setTitle("✨ GamingWithoutBorders Bot")
+      .setDescription("Use `&command` to run commands")
       .addFields(
 
         {
-          name: "General",
+          name: "📌 General",
           value:
-            "`&help`\n" +
-            "`&ping`\n" +
-            "`&say text`",
+            "🔹 `&help`\n" +
+            "🔹 `&ping`\n" +
+            "🔹 `&say text`",
         },
 
         {
-          name: "Moderation",
+          name: "🛡 Moderation",
           value:
-            "`&kick @user reason`\n" +
-            "`&ban @user reason`\n" +
-            "`&mute @user 10m reason`",
+            "🔹 `&kick @user reason`\n" +
+            "🔹 `&ban @user reason`\n" +
+            "🔹 `&mute @user 10m reason`",
         },
 
         {
-          name: "Setup",
+          name: "⚙️ Setup",
           value:
-            "`&setwelcome #channel`\n" +
-            "`&setlogs #channel`",
+            "🔹 `&setwelcome #channel`\n" +
+            "🔹 `&setlogs #channel`",
         },
 
         {
-          name: "Birthday",
+          name: "🎂 Birthday",
           value:
-            "`&setbirthday @user 22-3`",
+            "🔹 `&setbirthday @user 22-3`",
         },
 
         {
-          name: "Todo",
+          name: "📝 Todo",
           value:
-            "`&createtodo name`",
+            "🔹 `&createtodo name`",
         }
 
       )
       .setFooter({
-        text: "GamingWithoutBorders • Ultra Bot"
+        text: "GamingWithoutBorders • Ultra Bot 🚀"
       })
       .setTimestamp();
 
